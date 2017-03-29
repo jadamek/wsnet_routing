1596d97b33bc2a607d99e1a055efe37631d729fa
James Robinson <jrobinsonbass@gmail.com>
2017-03-29 12:36:31 -0400

    mac adaption

--- wsnet/models/mac/802_15_4_u_csma_ca.c
+++ wsnet/models/mac/802_15_4_u_csma_ca.c
@@ -16,6 +16,8 @@
 #define STATE_TX      3
 #define STATE_TXING   4
 
+#define COMP_TIME 50000
+
 
 /* ************************************************** */
 /* ************************************************** */
@@ -59,7 +61,7 @@ struct nodedata {
     int NB;
 
     void *packets;
-    packet_t *txbuf;
+    buffer_entry_t *txbuf;
 
     double EDThreshold;
     int cs;
@@ -174,13 +176,15 @@ int setnode(call_t *c, void *params) {
 
 int unsetnode(call_t *c) {
     struct nodedata *nodedata = get_node_private_data(c);
-    packet_t *packet;
-    while ((packet = (packet_t *) das_pop(nodedata->packets)) != NULL) {
-        packet_dealloc(packet);
+    buffer_entry_t *buff_ent;
+    while ((buff_ent = (buffer_entry_t *)das_pop(nodedata->packets)) != NULL) {
+        packet_dealloc(buff_ent->packet);
+	free(buff_ent);
     }
     das_destroy(nodedata->packets);    
     if (nodedata->txbuf) {
-        packet_dealloc(nodedata->txbuf);
+        packet_dealloc(nodedata->txbuf->packet);
+	free(nodedata->txbuf);
     }
     free(nodedata);
     return 0;
@@ -202,7 +206,7 @@ int check_channel_busy(call_t *c) {
     return 0;
 }
 
-int state_machine(call_t *c, void *args) { 
+int state_machine(call_t *c, void *args) {
     struct nodedata *nodedata = get_node_private_data(c);
     call_t c0 = {get_entity_bindings_down(c)->elts[0], c->node, c->entity};
     uint64_t timeout;
@@ -216,8 +220,8 @@ int state_machine(call_t *c, void *args) {
     switch (nodedata->state) {
 		
     case STATE_IDLE:
-        if (nodedata->txbuf == NULL) {
-            nodedata->txbuf = (packet_t *) das_pop_FIFO(nodedata->packets);
+        if (nodedata->txbuf == NULL){
+            nodedata->txbuf = (buffer_entry_t*)das_pop_FIFO(nodedata->packets);
             if (nodedata->txbuf == NULL) {
                 return 0;
             }
@@ -230,46 +234,46 @@ int state_machine(call_t *c, void *args) {
             backoff = get_random_double() * (pow(2, nodedata->BE) - 1) * aUnitBackoffPeriod;
 			
             nodedata->clock = get_time() + backoff;  
-            scheduler_add_callback(nodedata->clock, c, state_machine, NULL);
+            nodedata->txbuf->event = scheduler_add_callback(nodedata->clock, c, state_machine, NULL);
             return 0;	
         } else {
             nodedata->state = STATE_TX;
             nodedata->clock = get_time();  
-            scheduler_add_callback(nodedata->clock, c, state_machine, NULL);
+            nodedata->txbuf->event = scheduler_add_callback(nodedata->clock, c, state_machine, NULL);
             return 0;
         }		
 
     case STATE_BACKOFF:
-        if (check_channel_busy(c)) { 
+        if (check_channel_busy(c)) {
             if ((++nodedata->BE) > nodedata->MaxBE) {
                 nodedata->BE = nodedata->MaxBE;
-            } 
-            if (++nodedata->NB >= nodedata->MaxCSMABackoffs) {
-                packet_dealloc(nodedata->txbuf);            
+            }
+/*            if (++nodedata->NB >= nodedata->MaxCSMABackoffs) {
+printf("[RDO] Too many backoffs, dropping packet at %d\n", c->node);
+                packet_dealloc(nodedata->txbuf->packet);            
                 nodedata->txbuf = NULL;
 
                 nodedata->state = STATE_IDLE;
                 nodedata->clock = get_time();
                 state_machine(c,NULL);
                 return 0;
-            }
+            }*/
             backoff = get_random_double() * (pow(2, nodedata->BE) - 1) * aUnitBackoffPeriod;
             nodedata->clock = get_time() + backoff;
-            scheduler_add_callback(nodedata->clock, c, state_machine, NULL);
+            nodedata->txbuf->event = scheduler_add_callback(nodedata->clock, c, state_machine, NULL);
             return 0;
         }
-        
         nodedata->state = STATE_TX;
         nodedata->clock = get_time();  
         state_machine(c,NULL);
         return 0;
         
     case STATE_TX:
-        packet = nodedata->txbuf;
+        packet = nodedata->txbuf->packet;
+	free(nodedata->txbuf);
         nodedata->txbuf = NULL;
         timeout = packet->size * 8 * radio_get_Tb(&c0) + macMinSIFSPeriod;
         TX(&c0, packet); 
-
         nodedata->state = STATE_TXING;
         nodedata->clock = get_time() + timeout;
         scheduler_add_callback(nodedata->clock, c, state_machine, NULL);
@@ -311,30 +315,31 @@ int get_header_real_size(call_t *c) {
 /* ************************************************** */
 void tx(call_t *c, packet_t *packet) {
     struct nodedata *nodedata = get_node_private_data(c);
+    buffer_entry_t *buff_ent = malloc(sizeof(buffer_entry_t));
+    buff_ent->packet = packet;
     
-    das_insert(nodedata->packets, (void *) packet);
-
-    if (nodedata->state == STATE_IDLE) {
-        nodedata->clock = get_time();  
-        state_machine(c,NULL);
-    } 
+    nodedata->clock = get_time() + COMP_TIME;  
+    buff_ent->event = scheduler_add_callback(nodedata->clock, c, state_machine,
+	NULL);
+    das_insert(nodedata->packets, (void*)buff_ent); 
 }
 
 
 /* ************************************************** */
 /* ************************************************** */
 void rx(call_t *c, packet_t *packet) {
-    struct nodedata *nodedata = get_node_private_data(c);
+    //struct nodedata *nodedata = get_node_private_data(c);
     struct _802_15_4_header *header = (struct _802_15_4_header *) packet->data;
     array_t *up = get_entity_bindings_up(c);
     int i = up->size;
 
     /* cf p171 ref 802.15.4-2006: discard packet while in backoff */
-    if (nodedata->state != STATE_IDLE) {
+/*    if (nodedata->state != STATE_IDLE) {
+printf("[RDO] Node %d trying to send, dropping received packet\n", c->node);
         packet_dealloc(packet);
         return;
         
-    }
+    }*/
 
     if ((header->dst != c->node) && (header->dst != BROADCAST_ADDR)) {
         packet_dealloc(packet);
@@ -359,10 +364,49 @@ void rx(call_t *c, packet_t *packet) {
 
 /* ************************************************** */
 /* ************************************************** */
-mac_methods_t methods;
-/* = {rx, 
+
+//returns buffer
+void* get_buffer(call_t *call)
+{
+    struct nodedata *node_data = get_node_private_data(call);
+    if(node_data->txbuf != NULL)
+	das_insert(node_data->packets, (void*)node_data->txbuf);
+    return node_data->packets;
+}
+
+//sets buffer
+void set_buffer(call_t *call, void *new_buffer)
+{
+    struct nodedata *node_data = get_node_private_data(call);
+    das_destroy(node_data->packets);
+
+    //if mate found, buffer entry already deallocated in routing module
+    if(node_data->txbuf && node_data->txbuf->packet == NULL)
+    {
+	free(node_data->txbuf);
+	node_data->txbuf = NULL;
+
+	node_data->state = STATE_IDLE;
+	node_data->clock = get_time();
+	scheduler_add_callback(node_data->clock, call, state_machine, NULL);
+    }
+    else if(node_data->txbuf)
+	das_pop(new_buffer);
+
+    node_data->packets = new_buffer;
+    set_node_private_data(call, node_data);
+    return;
+}
+
+
+/* ************************************************** */
+/* ************************************************** */
+mac_methods_t methods
+ = {rx, 
                          tx,
                          set_header, 
                          get_header_size,
-                         get_header_real_size};
-*/
+                         get_header_real_size,
+			 get_buffer,
+			 set_buffer};
+
