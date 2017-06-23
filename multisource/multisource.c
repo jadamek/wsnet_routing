8a763b0681f5f4ec5a1178d88bbf50b5a13f104d
Jordan Adamek <jadamek2@kent.edu>
2017-06-23 05:00:40 -0400

    Multisource broadcast initial commit; multiple flooding finished.

---
+++ multisource/multisource.c
@@ -0,0 +1,315 @@
+// muisource.c
+// generic application that selects the correct number of source devices
+// and launches a routing call from each
+// Jordan Adamek
+// 
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <include/modelutils.h>
+
+////////////////////////////////////////////////////////////////////////////////
+// Model information
+
+model_t model =
+{
+	"Multisource Broadcast Application Module",
+	"Jordan Adamek",
+	"0.1",
+	MODELTYPE_APPLICATION,
+	{NULL, 0}
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// Defines
+
+#define TRUE 1
+#define FALSE 0
+
+#define DEFAULT_PACKET_SIZE 10
+#define TX_START_TIME 5000000000ull
+#define DEFAULT_START_TIME 0
+
+#define ERROR -1
+#define EMPTY_DESTINATION {-2, {-1, -1, -1}}
+#define THIS_DESTINATION(call) {call->node, *get_node_position(call->node)}
+
+#define CALL_DOWN(call) {get_entity_bindings_down(call)->elts[0], call->node,\
+	call->entity}
+#define NEW(type) malloc(sizeof(type))
+
+#define TIME_PRECISION 1000000000
+
+////////////////////////////////////////////////////////////////////////////////
+// Structs and Typedefs
+
+typedef struct
+{
+	int overhead;
+	int packet_size;
+	bool received_packet;
+}node_data_t;
+
+typedef struct
+{
+	destination_t source;
+	void* sources;
+	int num_sources;
+	int num_tx;
+	int num_rx;
+	uint64_t latency;
+}entity_data_t;
+
+////////////////////////////////////////////////////////////////////////////////
+// Prototypes
+
+int init(call_t *call, void *params);
+
+int destroy(call_t *call);
+
+int call_back(call_t *call, void *args);
+int tx_start(call_t *call, void *args);
+
+bool compare_destinations(destination_t *l, destination_t *r);
+bool node_present_in(void *das, destination_t *id);
+
+////////////////////////////////////////////////////////////////////////////////
+// Init
+
+int init(call_t *call, void *params)
+{
+    entity_data_t *entity_data = NEW(entity_data_t);
+    param_t *param;
+    destination_t empty_destination = EMPTY_DESTINATION;
+
+    //init values
+    int percentage = 0;
+    entity_data->source = empty_destination;
+    entity_data->sources = das_create();
+    entity_data->num_sources = 0;
+    entity_data->num_tx = 0;
+    entity_data->num_rx = 0;
+    entity_data->latency = 0;
+
+    //get parameters
+    das_init_traverse(params);
+    while((param = (param_t*)das_traverse(params)) != NULL)
+    {
+	if(!strcmp(param->key, "source"))
+	    get_param_nodeid(param->value, &(entity_data->source.id),
+		entity_data->source.id);
+	if(!strcmp(param->key, "num_sources"))
+	    get_param_integer(param->value, &(entity_data->num_sources));
+	if(!strcmp(param->key, "percent"))
+	    get_param_integer(param->value, &percentage);
+    }
+
+    if(percentage != 0){
+	entity_data->num_sources = ceil(get_node_count() *
+	    ((double)percentage / 100));
+	entity_data->num_rx = entity_data->num_sources;
+    }
+
+    das_insert(entity_data->sources, (void*)&entity_data->source);
+
+    if(entity_data->num_sources > 0){
+    	// Generate remaining source list
+        int i = 0;
+    	destination_t* tmp = 0;             
+
+    	for(i = 0; i < entity_data->num_sources; i++){
+            tmp = NEW(destination_t);      
+            tmp->id = i;            
+	    tmp->position.x = -1;
+	    tmp->position.y = -1;
+
+	    das_insert(entity_data->sources, (void*)tmp);
+    	}
+    }
+
+    set_entity_private_data(call, entity_data);
+    return 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Destroy
+
+int destroy(call_t *call)
+{
+    entity_data_t *entity_data = get_entity_private_data(call);
+    FILE *results;
+    char *name = (char*)malloc(11 * sizeof(int));
+    sprintf(name, "results.txt");
+
+    fprintf(stderr, "Application Statistics:\n");
+    fprintf(stderr, "  message sent from %d\n", entity_data->source.id);
+    fprintf(stderr, "  number of sources: %d\n", entity_data->num_sources);
+    fprintf(stderr, "  num messages sent: %d\n", entity_data->num_tx);
+    fprintf(stderr, "  num messages received: %d\n", entity_data->num_rx);
+    fprintf(stderr, "  message delivery rate: %f\n",
+	(double)entity_data->num_rx / (double)entity_data->num_tx);
+    fprintf(stderr, "  latency: %lld nanoseconds\n", entity_data->latency);
+
+    if((results = fopen(name, "a")) == NULL)
+	fprintf(stderr, "[ERR] Couldn't open file: results.txt\n");
+    else
+	fprintf(results, "%d\t%d\t%lld\t", entity_data->num_sources,
+	    entity_data->num_rx, entity_data->latency);
+    fclose(results);
+
+    free(entity_data);
+    return 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Set Node
+
+int setnode(call_t *call, void *params)
+{
+    node_data_t *node_data = NEW(node_data_t);
+    /*param_t *param*/
+
+    node_data->packet_size = DEFAULT_PACKET_SIZE;
+    node_data->received_packet = false;
+
+    /*
+    //get params
+    das_init_traverse(params);
+    while((param = (param_t*)das_traverse(params)) != NULL)
+    {
+    }
+    */
+
+    set_node_private_data(call, node_data);
+    return 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Unset Node
+
+int unsetnode(call_t *call)
+{
+    node_data_t *node_data = get_node_private_data(call);
+
+    free(node_data);
+    return 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Bootstrap
+
+int bootstrap(call_t *call)
+{
+    entity_data_t *entity_data = get_entity_private_data(call);
+    node_data_t *node_data = get_node_private_data(call);
+    int i = get_entity_links_down_nbr(call);
+    entityid_t *down = get_entity_links_down(call);
+    destination_t my_pos = THIS_DESTINATION(call);
+    node_data->overhead = 0;
+
+    //get overhead
+    while(i--)
+    {
+	call_t call_down = {down[i], call->node, call->entity};
+
+	if(get_entity_type(&call_down) == MODELTYPE_ROUTING ||
+	    get_entity_type(&call_down) == MODELTYPE_MAC)
+	    node_data->overhead += GET_HEADER_SIZE(&call_down);
+    }
+
+    if(node_present_in(entity_data->sources, &my_pos))
+	scheduler_add_callback(DEFAULT_START_TIME, call, call_back, NULL);
+    return 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// TX functions
+
+int call_back(call_t *call, void *args)
+{
+    entity_data_t *entity_data = get_entity_private_data(call);
+    node_data_t *node_data = get_node_private_data(call);
+    call_t call_down = CALL_DOWN(call);
+    destination_t num_sources = EMPTY_DESTINATION;
+    entity_data->source.position = *get_node_position(entity_data->source.id);
+
+    packet_t *packet = packet_create(call, node_data->packet_size +
+	node_data->overhead, -1);
+    num_sources.id = entity_data->num_sources;
+    num_sources.position.x = entity_data->source.id;
+
+    if(SET_HEADER(&call_down, packet, &num_sources) == ERROR)
+    {
+	fprintf(stderr, "[ERR] Couldn't route, error in module down from "
+	    "multisource broadcast\n");
+	packet_dealloc(packet);
+	return 0;
+    }
+
+    entity_data->num_tx++;
+    scheduler_add_callback(entity_data->num_sources * get_node_count() * TX_START_TIME, call,
+	tx_start, (void*)packet);
+    return 0;
+}
+
+int tx_start(call_t *call, void *args)
+{
+    call_t call_down = CALL_DOWN(call);
+    TX(&call_down, (packet_t*)args);
+    return 0;
+}
+
+int ioctl(call_t *call, int option, void *in, void **out)
+{
+    return 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// RX
+
+void rx(call_t *call, packet_t *packet)
+{
+    entity_data_t *entity_data = get_entity_private_data(call);
+    node_data_t *node_data = get_node_private_data(call);
+
+
+    if(!node_data->received_packet)
+    {
+	entity_data->latency = get_time() - (entity_data->num_sources *
+	    get_node_count() * TX_START_TIME);
+	node_data->received_packet = TRUE;
+	entity_data->num_rx++;
+    }
+    packet_dealloc(packet);
+    return;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Helper Functions
+
+bool compare_destinations(destination_t *l, destination_t *r)
+{
+    return l->id == r->id;
+}
+
+//checks whether or not a node is present in a given data structure
+bool node_present_in(void *das, destination_t *identify)               
+{
+    das_init_traverse(das);
+    destination_t *tmp = NULL;
+
+    while((tmp = (destination_t*)das_traverse(das)) != NULL)
+    {
+        if(compare_destinations(tmp, identify))
+            return true;
+    }
+    return false;                  
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Application Methods
+
+application_methods_t methods =
+{
+	rx
+};
